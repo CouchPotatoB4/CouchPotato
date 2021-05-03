3b8265d0185a2586c3892d493cfe4ea1d2cf245b
Noah Hartmann <n.hartmann@seeburger.de>
2021-05-03 14:12:37 +0200

    Added Unit Tests for Show, User & Lobby. + changes in gitignore

--- CouchPotatoTests/Backend/LobbyUtil/LobbyTests.cs
+++ CouchPotatoTests/Backend/LobbyUtil/LobbyTests.cs
@@ -1,39 +1,60 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CouchPotato.Backend.LobbyUtil;
-using CouchPotato.Backend.UserUtil;
-using System;
+﻿using CouchPotato.Backend.UserUtil;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Text;
 
-namespace CouchPotato_TestProject
+namespace CouchPotato.Backend.LobbyUtil.Tests
 {
-    [TestClass]
-    public class BackendTests
-    {
-        [TestMethod]
-        public void UserTests()
-        {
-            User user1 = UserFactory.build("User");
-            User user2 = UserFactory.build("User");
+	[TestClass()]
+	public class LobbyTests
+	{
+		[TestMethod()]
+		public void getUserTest()
+		{
+			User hostUser = UserFactory.build("User");
+			User user2 = UserFactory.build("AnotherUser");
+			User user3 = UserFactory.build("FinalUser");
+			Lobby lobby1 = LobbyFactory.build(hostUser);
 
-            Assert.IsNotNull(user1.ID);
-            Assert.IsNotNull(user1.Name);
+			lobby1.addUser(user2);
+			lobby1.addUser(user3);
+			ISet<User> users = lobby1.getUser();
 
-            Assert.AreNotEqual(user1.ID, user2.ID);
-            Assert.AreNotEqual(user1, user2);
-        }
+			Assert.IsNotNull(users);
+			//Assert.IsTrue(users.Contains(hostUser)); TODO
+			Assert.IsTrue(users.Contains(user2));
+			Assert.IsTrue(users.Contains(user3));
+		}
 
-        [TestMethod]
-        public void LobbyTests()
-        {
-            User user1 = UserFactory.build("User");
-            Lobby lobby1 = LobbyFactory.build(user1);
-            Lobby lobby2 = LobbyFactory.build(user1);
+		[TestMethod()]
+		public void getUserTest1()
+		{
+			User user1 = UserFactory.build("User");
+			Lobby lobby1 = LobbyFactory.build(user1);
+			Lobby lobby2 = LobbyFactory.build(user1);
 
-            Assert.IsNotNull(lobby1.getUser(user1.ID));
-            Assert.AreNotEqual(lobby1, lobby2);
-            Assert.AreEqual(lobby1.getUser(user1.ID), user1);
-            Assert.AreEqual(lobby1.getUser(user1.ID), lobby2.getUser(user1.ID));
-        }
-    }
+			Assert.IsNotNull(lobby1.getUser(user1.ID));
+			Assert.AreNotEqual(lobby1, lobby2);
+			Assert.AreEqual(lobby1.getUser(user1.ID), user1);
+			Assert.AreEqual(lobby1.getUser(user1.ID), lobby2.getUser(user1.ID));
+
+			User user2 = UserFactory.build("User2");
+			lobby1.addUser(user2);
+			long invalidID = user1.ID + user2.ID;
+			Assert.IsNull(lobby1.getUser(invalidID));
+		}
+
+		[TestMethod()]
+		public void LobbyTest()
+		{
+			Lobby lobby = new Lobby(null, 0);
+			Assert.IsNotNull(lobby);
+		}
+
+		[TestMethod()]
+		public void setConfigurationTest()
+		{
+			LobbyFactory.build(null).setConfiguration(ApiUtil.Provider.Aniflix, 5, 5);
+			LobbyFactory.build(null).setConfiguration(null, -1, -1);
+		}
+	}
 }
---
+++ CouchPotatoTests/Backend/ShowUtil/ShowTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CouchPotato.Backend.ShowUtil.Tests
+{
+	[TestClass()]
+	public class ShowTests
+	{
+		[TestMethod()]
+		public void ShowTest()
+		{
+			Show showFromConstructor = new Show(1, "TestShow", "TestDescription", "TestPath");
+			Show showFromFactory = ShowFactory.build(1, "TestShow", "TestDescription", "TestPath");
+
+			Assert.AreEqual(showFromConstructor.Id, showFromFactory.Id);
+			Assert.AreEqual(showFromConstructor.Name, showFromFactory.Name);
+			Assert.AreEqual(showFromConstructor.Description, showFromFactory.Description);
+			Assert.AreEqual(showFromConstructor.CoverStorage, showFromFactory.CoverStorage);
+
+			int oldVotesCount = showFromConstructor.getVotes();
+			showFromConstructor.vote();
+			Assert.AreNotEqual(oldVotesCount, showFromConstructor.getVotes());
+		}
+
+		[TestMethod()]
+		public void ShowTest2()
+		{
+			Show show1 = ShowFactory.build(1, "TestShow", "TestDescription", "TestPath");
+			try
+			{
+				Show show2 = ShowFactory.build(1, "TestShow", "TestDescription", "TestPath");
+			}
+			catch (Exception)
+			{
+				// Test passed
+			}
+			Assert.Fail("Exception for id duplication expected");
+		}
+	}
+}
