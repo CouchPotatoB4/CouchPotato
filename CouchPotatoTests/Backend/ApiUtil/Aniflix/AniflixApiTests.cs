3d8fadc8e063e421ea54a591e27076d4144df016
Noah Hartmann <n.hartmann@seeburger.de>
2021-05-05 16:49:44 +0200

    First attempt on reworking the aniflix api tests

--- CouchPotatoTests/Backend/ApiUtil/Aniflix/AniflixApiTests.cs
+++ CouchPotatoTests/Backend/ApiUtil/Aniflix/AniflixApiTests.cs
@@ -3,19 +3,70 @@ using CouchPotato.Backend.ApiUtil.Aniflix;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
+using CouchPotato.Backend.ShowUtil;
 
 namespace CouchPotato.Backend.ApiUtil.Aniflix.Tests
 {
 	[TestClass()]
 	public class AniflixApiTests
 	{
-		[TestMethod()]
-		public void AniflixApiConnectionTest()
+        private class PseudoAniflixApi : AniflixApi
+        {
+			internal PseudoAniflixApi(Boolean fillWithPseudoData) : base()
+            {
+                Genre[] testGenres = new Genre[10];
+				Show[] testShows = new Show[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    testGenres[i] = VotableFactory.build("Genre");
+                    testShows[i] = VotableFactory.build(1, "Show", "Description", "Path");
+                }
+
+                base.genres = testGenres;
+                base.shows = testShows;
+            }
+
+            internal bool isHTTPStatusCodeOk()
+            {
+                return base.isStatusCodeOk();
+            }
+        }
+
+        [TestMethod()]
+		public void GeneralApiTest()
 		{
-			AniflixApi api = new AniflixApi();
-			Assert.AreEqual(200, api.getStatusCode());
-			Assert.IsNotNull(api.getGenres());
-			Assert.IsNotNull(api.getShows());
+            PseudoAniflixApi filledApi = new PseudoAniflixApi(true);
+            Genre[] genres = filledApi.getGenres();
+            Assert.IsNotNull(genres);
+            Assert.IsNotNull(filledApi.getShows());
+            Assert.IsNotNull(filledApi.getShows(VotableFactory.build("Adventure")));
+            Assert.IsNotNull(filledApi.getShows(0));
+            Assert.IsNotNull(filledApi.getShows(new HashSet<Genre>()));
 		}
+
+        [TestMethod()]
+        public void ApiConnectionTest()
+        {
+            PseudoAniflixApi emptyApi = new PseudoAniflixApi(false);
+
+            if (emptyApi.isHTTPStatusCodeOk())
+            {
+                Assert.IsNotNull(emptyApi.getGenres());
+                Assert.IsNotNull(emptyApi.getShows());
+                return;
+            }
+            
+            try
+            {
+                emptyApi.getGenres();
+            }
+            catch(Exception)
+            {
+                // test passed
+                return;
+            }
+            Assert.Fail("Denied Connection Exception expected!");
+        }
 	}
 }
