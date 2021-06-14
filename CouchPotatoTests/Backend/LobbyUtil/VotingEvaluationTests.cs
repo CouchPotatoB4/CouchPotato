28cba73fce466a80babea73c909f524f4ed81925
Raziel Engel <raziel.engel@gmx.de>
2021-06-14 14:47:02 +0200

    Fixed the VotingEvaluation and the Test

--- CouchPotatoTests/Backend/LobbyUtil/VotingEvaluationTests.cs
+++ CouchPotatoTests/Backend/LobbyUtil/VotingEvaluationTests.cs
@@ -12,7 +12,7 @@ namespace CouchPotato.Backend.LobbyUtil.Tests
     {
 
         [TestMethod()]
-        public void CompareTest()
+        public void CompareHighestTest()
         {
             VotingEvaluation evaluation = new VotingEvaluation();
             Genre genre1 = VotableFactory.buildGenre("Adventure");
@@ -20,11 +20,79 @@ namespace CouchPotato.Backend.LobbyUtil.Tests
             genre1.Vote();
             Genre genre2 = VotableFactory.buildGenre("Love Story");
             genre2.Vote();
+            Genre genre3 = VotableFactory.buildGenre("Horror");
 
-            Assert.AreEqual(1, evaluation.Compare(genre1, genre2));
+            ISet<Genre> set = new HashSet<Genre>();
+            set.Add(genre1);
+            set.Add(genre2);
+            set.Add(genre3);
 
+            ISet<Genre> expectedHighest = new HashSet<Genre>();
+            expectedHighest.Add(genre1);
+
+            Assert.IsTrue(containtsSame(expectedHighest, evaluation.evaluateGenre(set, EvaluationType.HIGHEST)));
+        }
+
+        [TestMethod()]
+        public void CompareVotedTest()
+        {
+            VotingEvaluation evaluation = new VotingEvaluation();
+            Genre genre1 = VotableFactory.buildGenre("Adventure");
+            genre1.Vote();
+            genre1.Vote();
+            Genre genre2 = VotableFactory.buildGenre("Love Story");
             genre2.Vote();
-            Assert.AreEqual(0, evaluation.Compare(genre1, genre2));
+            Genre genre3 = VotableFactory.buildGenre("Horror");
+
+            ISet<Genre> set = new HashSet<Genre>();
+            set.Add(genre1);
+            set.Add(genre2);
+            set.Add(genre3);
+
+            ISet<Genre> expectedVoted = new HashSet<Genre>();
+            expectedVoted.Add(genre1);
+            expectedVoted.Add(genre2);
+
+            Assert.IsTrue(containtsSame(expectedVoted, evaluation.evaluateGenre(set, EvaluationType.VOTED)));
+        }
+
+        [TestMethod()]
+        public void CompareAllTest()
+        {
+            VotingEvaluation evaluation = new VotingEvaluation();
+            Genre genre1 = VotableFactory.buildGenre("Adventure");
+            genre1.Vote();
+            genre1.Vote();
+            Genre genre2 = VotableFactory.buildGenre("Love Story");
+            genre2.Vote();
+            Genre genre3 = VotableFactory.buildGenre("Horror");
+
+            ISet<Genre> set = new HashSet<Genre>();
+            set.Add(genre1);
+            set.Add(genre2);
+            set.Add(genre3);
+
+            ISet<Genre> expectedAll = new HashSet<Genre>();
+            expectedAll.Add(genre1);
+            expectedAll.Add(genre2);
+            expectedAll.Add(genre3);
+
+            Assert.IsTrue(containtsSame(expectedAll, evaluation.evaluateGenre(set, EvaluationType.ALL)));
+        }
+
+        private bool containtsSame(ISet<Genre> expected, ISet<Genre> actual)
+        {
+            if (expected.Count != actual.Count) return false;
+
+            var it = actual.GetEnumerator();
+
+            foreach(Genre v in expected)
+            {
+                if (v.Equals(it.Current)) return false;
+                it.MoveNext();
+            }
+
+            return true;
         }
     }
 }
